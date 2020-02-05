1244c727d3404fe0a967d9470732c21bb03ff867
Matthew Ha <hello.matthewha@gmail.com>
2020-02-04 19:52:19 -0800

    finishing update

--- src/config.py
+++ src/config.py
@@ -3,6 +3,5 @@
 
 BYTE_ORDER = 'little'
 PAGE_RANGE_MAX_BASE_PAGES = 16 # max columns
-PAGE_RANGE_MAX_SIZE = 200 # 150 tail pages possible
 CELLS_PER_PAGE = 500
 CELL_SIZE_BYTES = round(4096 / CELLS_PER_PAGE)
