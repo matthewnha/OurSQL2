fa19d2cfbf3a5b03fb75a91eb0b36926d4875d11
DavidECerda <Decerda@ucdavis.edu>
2020-02-05 00:23:37 -0800

    merge

--- src/maintest.py
+++ src/maintest.py
@@ -11,7 +11,7 @@ grades_table = db.create_table('Grades', 5, 0)
 query = Query(grades_table)
 
 inserted = []
-for i in range(0,300):
+for i in range(10000):
     cols = [947032+i, randint(0,20), randint(0,20), randint(0,20), randint(0,20)]
     query.insert(*cols)
     inserted.append(cols)
@@ -23,13 +23,8 @@ if fetched != row:
 else:
     print('Matched', fetched, row)
 
-#
-#
-# query.insert(9399394, 1, 2, 3, 4)
-#query.update(9399394, [None, None, 6, None, None])
-# query.update(9399394, [None, 10, None, None, None])
-# query.update(9399394, [None, 11, None, None, 9])
-#query.insert(9397395, 4, 6, 7, 8)
+for
+
 query.insert(9399395, 5, 6, 7, 8)
 query.update(9399395, [None, 99, 88, None, None])
 query.update(9399395, [None, 100, None, None, None])
