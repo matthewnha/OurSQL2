98f06de8bf1b2723fcee9d933f1a58aa13dea154
DavidECerda <Decerda@ucdavis.edu>
2020-02-04 22:52:26 -0800

    sum worked on

--- src/maintest.py
+++ src/maintest.py
@@ -10,20 +10,20 @@ db = Database()
 grades_table = db.create_table('Grades', 5, 0)
 query = Query(grades_table)
 
-# inserted = []
-# for i in range(300):
-#   cols = [947032+i, randint(0,20), randint(0,20), randint(0,20), randint(0,20)]
-#   query.insert(*cols)
-#   inserted.append(cols)
+inserted = []
+for i in range(300):
+    cols = [947032+i, randint(0,20), randint(0,20), randint(0,20), randint(0,20)]
+    query.insert(*cols)
+    inserted.append(cols)
 
-# for row in inserted:
-#   fetched = query.select(row[0], [1,1,1,1,1])
-#   if fetched != row:
-#     raise Exception('Not match', fetched, row)
-#   else:
-#     print('Matched', fetched, row)
+for row in inserted:
+    fetched = query.select(row[0], [1,1,1,1,1])
+if fetched != row:
+    raise Exception('Not match', fetched, row)
+else:
+    print('Matched', fetched, row)
 
-query.insert(9399394, 1, 2, 3, 4)
+#query.insert(9399394, 1, 2, 3, 4)
 # query.update(9399394, [None, None, 6, None, None])
 # query.update(9399394, [None, 10, None, None, None])
 # query.update(9399394, [None, 11, None, None, 9])
@@ -33,7 +33,7 @@ query.insert(9399394, 1, 2, 3, 4)
 # query.update(9399395, [None, 100, None, None, None])
 # query.update(9399395, [None, None, None, None, 200])
 
-print(query.select(9399394, [1,1,1,1,1])[0])
+print(query.sum(947032,947100,3))
 # print(query.select(9399395, [1,1,1,1,1])[0])
 
-query.delete(9399394)
+#query.delete(9399394)
