6f82b5cae340ee6aaf8b5ba7fb984c3b3194f67a
Matthew Ha <hello.matthewha@gmail.com>
2020-02-05 17:52:05 -0800

    fix table constructor

--- src/table.py
+++ src/table.py
@@ -34,7 +34,7 @@ class Table:
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
     """
-    def __init__(self, name, key_col, num_columns):
+    def __init__(self, name, num_columns, key_col):
         self.name = name
         self.key_col = key_col
         self.num_columns = num_columns
