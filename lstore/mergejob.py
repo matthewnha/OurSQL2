a6b7b3462acb209456480e8ef9810dda4b8525ac
DavidECerda <Decerda@ucdavis.edu>
2020-02-22 14:26:44 -0800

    merge record writteb

--- lstore/mergejob.py
+++ lstore/mergejob.py
@@ -1,58 +1,102 @@
-from table import Table
+from table import *
 from config import *
-
+from page import Page
+from util import *
 
 class MergeJob:
 
     def __init__(self, table):
-        self.base_pages = {}
+        self.copied_metarecords = {}
+        self.copied_base_pages = {}
+        self.copied_prev_rid = None
         self.table = table # type: Table
 
-    def copy_base_pages(self,table):
-        pass
+    def copy_data(self):
+        self.copied_prev_rid = self.table.prev_rid
+        copied_metarecords = {}
+        copied_base_pages = {}
+        for rid in range(1, self.copied_prev_rid+1):
+            current_record = self.table.page_directory[rid].copy()
+           
+            for pid in current_record.columns:
+                cell_idx, inner_idx, range_idx = pid
+
+                if (inner_idx, range_idx) not in copied_base_pages:
+                    copied_base_pages[(inner_idx, range_idx)] = self.table.get_page(pid)
+                
+            copied_metarecords[rid] = current_record
+
+        return [copied_metarecords, copied_base_pages]
+        
+
+    def read_copied_by_pid(self, pid):
+        cell_idx, inner_idx, range_idx = pid
+        page = self.copied_base_pages[(inner_idx, range_idx)] # type: Page
+        bytes_read = page.read(cell_idx)
+        return bytes_read
 
     def collapse_record(self, rid):
-        base_record = self.table.page_directory[rid] # type: MetaRecord
-        USER_COLS_START = self.table.START_USER_DATA_COLUMN
-        for pid in base_record.columns[USER_COLS_START:]:
-            pass
+        base_record = self.copied_metarecords[rid] # type: MetaRecord
+        USER_COLS_START = START_USER_DATA_COLUMN
+        # for pid in base_record.columns[USER_COLS_START:]:
+        #     pass
 
 
         resp = [None for _ in range(self.table.num_columns)]
-        resp = [1 for _ in range(self.table.num_columns)]
+        need = [1 for _ in range(self.table.num_columns)]
 
         base_enc_pid = base_record.columns[SCHEMA_ENCODING_COLUMN]
-        base_enc_bytes = self.table.read_pid(base_enc_pid)
-        base_enc_binary = bin(int_from_bytes(base_enc_bytes))[2:].zfill(self.num_columns)
+        base_enc_bytes = self.read_copied_by_pid(base_enc_pid)
+        base_enc_binary = bin(int_from_bytes(base_enc_bytes))[2:].zfill(self.table.num_columns)
 
         for data_col_idx, is_dirty in enumerate(base_enc_binary):
             if is_dirty == '1' or need[data_col_idx] == 0:
                 continue
             col_pid = base_record.columns[START_USER_DATA_COLUMN + data_col_idx]
-            data = self.read_pid(col_pid)
+            data = self.read_copied_by_pid(col_pid)
             resp[data_col_idx] = int_from_bytes(data)
             need[data_col_idx] = 0
 
-        curr_record = base_record
 
-        while indirection != base_rid or indirection > tsp:
+        # read base record
+        curr_indir_pid = base_record.columns[INDIRECTION_COLUMN]
+        next_rid = self.read_copied_by_pid(curr_indir_pid)
+        next_rid = int_from_bytes(next_rid)
+        curr_record = self.table.page_directory[next_rid] # Reading tails from real data in table now
+
+        curr_enc_pid = curr_record.columns[SCHEMA_ENCODING_COLUMN]
+        curr_enc_bytes = self.table.read_pid(curr_enc_pid)
+        curr_enc = int_from_bytes(curr_enc_bytes)
+        curr_enc_binary = bin(curr_enc)[2:].zfill(self.table.num_columns)
+
+        for data_col_idx, is_updated in enumerate(curr_enc_binary):
+            #print("still needs",need,"curr schema",curr_enc_binary,"dex", data_col_idx, "at", is_updated)
+            if is_updated == '0' or need[data_col_idx] == 0:
+                continue
+            col_pid = curr_record.columns[START_USER_DATA_COLUMN + data_col_idx]
+            data = self.table.read_pid(col_pid)
+            data = int_from_bytes(data)
+            resp[data_col_idx] = data
+            need[data_col_idx] = 0
+
+        # read tail records
+        while sum(need) != 0: #  todo: or indirection > tps or more?
             curr_indir_pid = curr_record.columns[INDIRECTION_COLUMN]
-            next_rid = self.read_pid(curr_indir_pid)
+            next_rid = self.table.read_pid(curr_indir_pid)
             next_rid = int_from_bytes(next_rid)
-            curr_record = self.page_directory[next_rid]
+            curr_record = self.table.page_directory[next_rid]
 
             curr_enc_pid = curr_record.columns[SCHEMA_ENCODING_COLUMN]
-            curr_enc_bytes = self.read_pid(curr_enc_pid)
+            curr_enc_bytes = self.table.read_pid(curr_enc_pid)
             curr_enc = int_from_bytes(curr_enc_bytes)
-            curr_enc_binary = bin(curr_enc)[2:].zfill(self.num_columns)
-            #print(next_rid, "next", curr_enc_pid, "schema", curr_enc_binary, "order")
-
+            curr_enc_binary = bin(curr_enc)[2:].zfill(self.table.num_columns)
+ 
             for data_col_idx, is_updated in enumerate(curr_enc_binary):
                 #print("still needs",need,"curr schema",curr_enc_binary,"dex", data_col_idx, "at", is_updated)
                 if is_updated == '0' or need[data_col_idx] == 0:
                     continue
                 col_pid = curr_record.columns[START_USER_DATA_COLUMN + data_col_idx]
-                data = self.read_pid(col_pid)
+                data = self.table.read_pid(col_pid)
                 data = int_from_bytes(data)
                 resp[data_col_idx] = data
                 need[data_col_idx] = 0
@@ -61,10 +105,11 @@ class MergeJob:
             
 
     def run(self):
+        
+        # todo: lock
+        self.copied_metarecords, self.copied_base_pages = self.copy_data()
 
-
-        for rid in range(self.table.prev_rid):
-            collapse_record(rid)
-            
+        for rid in range(1, self.copied_prev_rid+1):
+            self.collapse_record(rid)
 
         pass
