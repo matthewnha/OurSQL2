26260d8441a246b4dd00ebf25bf2fab416a160b8
DavidECerda <Decerda@ucdavis.edu>
2020-02-21 16:19:54 -0800

    Added all tester files

--- lstore/db.py
+++ lstore/db.py
@@ -19,6 +19,7 @@ class Database():
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
     """
+
     def create_table(self, name, num_columns, key):
         table = Table(name, num_columns, key)
         return table
@@ -27,4 +28,9 @@ class Database():
     # Deletes the specified table
     """
     def drop_table(self, name):
+        pass
+
+     # Returns table with the passed name
+
+    def get_table(self, name):
         pass
