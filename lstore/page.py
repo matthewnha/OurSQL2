b03c53b6e2bc70e755791854c55fdc5f5369448c
Matthew Ha <mnha@ucdavis.edu>
2020-02-23 21:05:04 -0800

    merge done

--- lstore/page.py
+++ lstore/page.py
@@ -9,7 +9,7 @@ class Page:
             self.data = None
         else:
             self.data = bytearray(4096)
-        self.cellSize = (4096 // BLOCKS_PER_PAGE)
+        self.write_tps(RESERVED_TID)
 
     def has_capacity(self):
         return self.num_records < (CELLS_PER_PAGE)
@@ -26,11 +26,11 @@ class Page:
         if not self.has_capacity():
             raise Exception('page is full')
 
-        start = (self.num_records + 1) * self.cellSize
-        end = start + self.cellSize
-        if len(value) != self.cellSize:
+        start = (self.num_records + 1) * CELL_SIZE_BYTES
+        end = start + CELL_SIZE_BYTES
+        if len(value) != CELL_SIZE_BYTES:
             value = int.from_bytes(value,'little')
-            value = value.to_bytes(self.cellSize,'little')
+            value = value.to_bytes(CELL_SIZE_BYTES,'little')
         self.data[start:end] = value
         self.num_records += 1
         return self.num_records
@@ -43,24 +43,24 @@ class Page:
             value: Must be bytes of the specified CELLS_PER_PAGE size
         '''
 
-        start = (cell_idx + 1) * self.cellSize
-        end = start + self.cellSize
-        if len(value) != self.cellSize:
+        start = (cell_idx + 1) * CELL_SIZE_BYTES
+        end = start + CELL_SIZE_BYTES
+        if len(value) != CELL_SIZE_BYTES:
             value = int.from_bytes(value,'little')
-            value = value.to_bytes(self.cellSize,'little')
+            value = value.to_bytes(CELL_SIZE_BYTES,'little')
         self.data[start:end] = value
         
 
         return self.num_records
 
     def write_tps(self, tid):
-        bytes_to_write = tid.to_bytes(self.cellSize,'little')
+        bytes_to_write = tid.to_bytes(CELL_SIZE_BYTES,'little')
         start = 0
-        end = start + self.cellSize
+        end = start + CELL_SIZE_BYTES
         self.data[start:end] = bytes_to_write
 
     def read_tps(self) -> int:
-        return int_from_bytes(bytes(self.data[0:self.cellSize]))
+        return int_from_bytes(bytes(self.data[0:CELL_SIZE_BYTES]))
 
 
     def read(self, cellIndex):
@@ -71,8 +71,8 @@ class Page:
         if cellIndex > CELLS_PER_PAGE - 1:
             raise Exception('cellIndex exceeds page size')
 
-        start = (cellIndex + 1) * self.cellSize
-        end = start + self.cellSize
+        start = (cellIndex + 1) * CELL_SIZE_BYTES
+        end = start + CELL_SIZE_BYTES
         return bytes(self.data[start:end])
 
     def copy(self):
--- lstore/table.py
+++ lstore/table.py
@@ -367,7 +367,7 @@ class Table:
                 data = self.read_pid(col_pid)
                 resp[data_col_idx] = int_from_bytes(data)
 
-                if not is_dirty:
+                if is_dirty == '0':
                     need[data_col_idx] = 0
 
             # get RID of next tail record
@@ -390,11 +390,11 @@ class Table:
                     if is_updated == '0':
                         continue
                     
-                    if next_rid <= tps_all[data_col_idx]:
-                        print('skipped bc merged')
+                    if next_rid >= tps_all[data_col_idx]:
                         need[data_col_idx] = 0
                         continue
 
+                    # print('LOOKED AT TAIL')
 
                     col_pid = curr_record.columns[START_USER_DATA_COLUMN + data_col_idx]
                     data = self.read_pid(col_pid)
@@ -402,11 +402,12 @@ class Table:
                     resp[data_col_idx] = data
                     need[data_col_idx] = 0
 
-                curr_indir_pid = curr_record.columns[INDIRECTION_COLUMN]
-                next_rid = int_from_bytes(self.read_pid(curr_indir_pid))
+                if sum(need) != 0:
+                    curr_indir_pid = curr_record.columns[INDIRECTION_COLUMN]
+                    next_rid = int_from_bytes(self.read_pid(curr_indir_pid))
 
-                if next_rid == rid: # if next rid is base
-                    raise Exception('Came back to original')
+                    if next_rid == rid: # if next rid is base
+                        raise Exception("Came back to original, didn't get all we needed")
 
             # Release locks and return
             release_all(locks)
